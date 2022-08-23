eb5dcdcc1e37175b1ca76ff7f835779afdb1dc03
dhng22 <gm2212002@gmail.com>
2022-08-23 13:56:55 +0700

    inital finish

--- app/src/main/java/com/teasoft/appusageutils/MainActivity.kt
+++ app/src/main/java/com/teasoft/appusageutils/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.teasoft.appusageutils
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import com.teasoft.utils.AppUsage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+        resultLauncher.launch(intent)
+
     }
+
+    val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        val appUsage = AppUsage.queryUsageTime(this, false)
+
+        for (appUsedTime in appUsage) {
+            Log.e("TAG", "${appUsedTime.totalUsedTime/1000/60} minutes ${appUsedTime.packageName} from ${appUsedTime.timeStampStart}, isDaily:${appUsedTime.isDaily}")
+        }
+    }
+
 }
--- utils/src/main/java/com/teasoft/models/AppUsedTime.kt
+++ utils/src/main/java/com/teasoft/models/AppUsedTime.kt
@@ -1,18 +1,18 @@
 package com.teasoft.models
 
 
-class AppUsedTime() {
+class AppUsedTime {
     var packageName: String? = null
     private var startPoint: Long = 0
     var totalUsedTime: Long = 0
     var timeStampStart: String? = null
     var isDaily: Boolean = true
 
-    fun startUsingPoint(stamp: Long): Unit {
+    internal fun startUsingPoint(stamp: Long): Unit {
         this.startPoint = stamp
     }
 
-    fun endUsingPoint(stamp: Long): Unit {
+    internal fun endUsingPoint(stamp: Long): Unit {
         totalUsedTime += (stamp - startPoint)
     }
 }
